4d53649110949d5a04a5a4537a70cbd022fc1dfe
DoriMG <15670245+DoriMG@users.noreply.github.com>
2025-08-04 12:36:45 +0200

    S Fig 4

--- R/hpc_sfig4.R
+++ R/hpc_sfig4.R
@@ -5,98 +5,108 @@ library(patchwork)
 library(lme4)
 library(lmerTest)
 library(emmeans)
-
+library("dplyr")
+library(rstatix)
 
 folder = r"{D:\Frankfurt\Papers\Sussex\hippocampal_pops\v3\matlab\data}"
 out_folder = r"{D:\Frankfurt\Papers\Sussex\hippocampal_pops\v3\R\figures}"  
 
-
+# Variable names
 nov_names <- c(
   `FALSE` = "Familiar" ,
-  `TRUE` = "Novel"
+  `TRUE` = "Shifted"
 )
 
+sal_names <- c(
+  `FALSE` = "Sparse" ,
+  `TRUE` = "Abundant"
+)
 
-## S Fig 2A - Place field width
-dataset = readMat(file.path(folder,"allAvePfs.mat"))
+loc_names <- c(
+  `1` = "Before obj." ,
+  `2` = "Around obj.",
+  `3` = "After obj."
+)
+
+## S Fig 4A - Place field width
+dataset = readMat(file.path(folder,"allAvePfs_ovc.mat"))
 
 perf_data = dataset[[1]]
-perf_data <- data.table( "value" = perf_data[,1], "dataset" = perf_data[,2], "env" = perf_data[,3], "training" = perf_data[,4], "mouse" = perf_data[,5])
+perf_data <- data.table( "value" = perf_data[,1], 
+                         "dataset" = perf_data[,2], 
+                         "env" = perf_data[,3], "training" = perf_data[,4], "mouse" = perf_data[,5])
 perf_data$nov = perf_data$env >2
 perf_data$sal = perf_data$env ==2 | perf_data$env ==4
+perf_data =  perf_data[perf_data$nov]
 perf_data = perf_data[perf_data$training==2]
 
-pf_plot = ggplot(data=perf_data[perf_data$env<5], aes(x=nov, y=value, fill=factor(sal))) +
+pf_plot = ggplot(data=perf_data, aes(x=sal, y=value, fill=sal)) +
   stat_summary(fun=mean, geom='bar', alpha=1, position = position_dodge(width=0.8), width=0.8) +
   stat_summary(fun.data = mean_cl_normal, geom="errorbar", width=0.3, position = position_dodge(width=0.8)) +
-  geom_point(position=position_jitterdodge(dodge.width = 0.8, jitter.width = 0.2), alpha=0.5, stroke = 0,shape=16,size=1) + 
-  scale_x_discrete(breaks=c(FALSE,TRUE),
-                   labels=nov_names) + 
+  geom_point(position=position_jitter(width = 0.2), alpha=0.5, stroke = 0,shape=16,size=1) +  
+  scale_x_discrete(breaks=c(FALSE, TRUE),
+                   labels=c("Sparse","Abundant"))+
   scale_fill_manual(labels = c('Sparse', 'Abundant'),values = c("#FFC19C", "#C59AD4"))+
-  labs(y ='Place field width (cm)', x=NULL, fill=NULL)
+  labs(y ='Object-vector field width', x=NULL, fill=NULL)
 pf_plot
 
-# Stats
-summary(perf_data)
-lmm = lmer(value ~ sal*nov +(1|mouse)+(1|dataset), data =perf_data)
+# stats
+lmm = lmer(value ~ sal +(1|mouse) +(1|dataset), data =perf_data)
 anova(lmm)
 
-write.csv(anova(lmm), "test.csv")
 
-
-## S Fig 2B- Place field stability
-dataset = readMat(file.path(folder,"allAveStab.mat"))
+## S Fig 4B- In/out ratio
+dataset = readMat(file.path(folder,"allIO_ovc.mat"))
 
 perf_data = dataset[[1]]
 perf_data <- data.table( "value" = perf_data[,1], "dataset" = perf_data[,2], "env" = perf_data[,3], "training" = perf_data[,4], "mouse" = perf_data[,5])
 perf_data$nov = perf_data$env >2
 perf_data$sal = perf_data$env ==2 | perf_data$env ==4
+perf_data =  perf_data[perf_data$nov]
 perf_data = perf_data[perf_data$training==2]
 
-stab_plot = ggplot(data=perf_data[perf_data$env<5], aes(x=nov, y=value, fill=factor(sal))) +
+io_plot =ggplot(data=perf_data, aes(x=sal, y=value, fill=sal)) +
   stat_summary(fun=mean, geom='bar', alpha=1, position = position_dodge(width=0.8), width=0.8) +
   stat_summary(fun.data = mean_cl_normal, geom="errorbar", width=0.3, position = position_dodge(width=0.8)) +
-  geom_point(position=position_jitterdodge(dodge.width = 0.8, jitter.width = 0.2), alpha=0.5, stroke = 0,shape=16,size=1) + 
-  scale_x_discrete(breaks=c(FALSE,TRUE),
-                   labels=nov_names) + 
-  scale_fill_manual(labels = c('Sparse', 'Abundant'),values = c("#FFC19C", "#C59AD4"))+
-  labs(y ='Stability', x=NULL, fill=NULL)
-stab_plot
-
-# Stats
-lmm = lmer(value ~ sal*nov +(1|mouse) +(1|dataset), data =perf_data)
-anova(lmm)
-aggregate(value ~ training  + (1|mouse), perf_data, function(x) c(mean = mean(x), sd = sd(x),length = length(x)))
-write.csv(anova(lmm), "test.csv")
-
-## S Fig 2C- In/out activity ratio
-dataset = readMat(file.path(folder,"allIO.mat"))
-
-perf_data = dataset[[1]]
-perf_data <- data.table( "value" = perf_data[,1], "dataset" = perf_data[,2], "env" = perf_data[,3], "training" = perf_data[,4], "mouse" = perf_data[,5])
-perf_data$nov = perf_data$env >2
-perf_data$sal = perf_data$env ==2 | perf_data$env ==4
-perf_data = perf_data[perf_data$training==2]
-
-io_plot = ggplot(data=perf_data[perf_data$env<5], aes(x=nov, y=value, fill=factor(sal))) +
-  stat_summary(fun=mean, geom='bar', alpha=1, position = position_dodge(width=0.8), width=0.8) +
-  stat_summary(fun.data = mean_cl_normal, geom="errorbar", width=0.3, position = position_dodge(width=0.8)) +
-  geom_point(position=position_jitterdodge(dodge.width = 0.8, jitter.width = 0.2), alpha=0.5, stroke = 0,shape=16,size=1) + 
-  scale_x_discrete(breaks=c(FALSE,TRUE),
-                   labels=nov_names) + 
+  geom_point(position=position_jitter(width = 0.2), alpha=0.5, stroke = 0,shape=16,size=1) +  
+  scale_x_discrete(breaks=c(FALSE, TRUE),
+                   labels=c("Sparse","Abundant"))+
   scale_fill_manual(labels = c('Sparse', 'Abundant'),values = c("#FFC19C", "#C59AD4"))+
   labs(y ='Out/in ratio', x=NULL, fill=NULL)
 io_plot
 
-# Stats
-data_temp = perf_data[perf_data$env<5]
-lmm = lmer(value ~ sal*nov +(1|mouse)+(1|dataset), data =data_temp)
+# stats
+lmm = lmer(value ~ sal +(1|mouse)+(1|dataset), data =data_temp)
 anova(lmm)
-aggregate(value ~ training  + (1|mouse), perf_data, function(x) c(mean = mean(x), sd = sd(x),length = length(x)))
-write.csv(anova(lmm), "test.csv")
+
+## S Fig 4C- Stability
+dataset = readMat(file.path(folder,"allAveStab_ovc.mat"))
+
+perf_data = dataset[[1]]
+perf_data <- data.table( "value" = perf_data[,1], "dataset" = perf_data[,2], "env" = perf_data[,3], 
+                         "training" = perf_data[,4], "mouse" = perf_data[,5])
+perf_data$nov = perf_data$env >2
+perf_data$sal = perf_data$env ==2 | perf_data$env ==4
+perf_data =  perf_data[perf_data$nov]
+perf_data = perf_data[perf_data$training==2]
+
+stab_plot =ggplot(data=perf_data, aes(x=sal, y=value, fill=sal)) +
+  stat_summary(fun=mean, geom='bar', alpha=1, position = position_dodge(width=0.8), width=0.8) +
+  stat_summary(fun.data = mean_cl_normal, geom="errorbar", width=0.3, position = position_dodge(width=0.8)) +
+  geom_point(position=position_jitter(width = 0.2), alpha=0.5, stroke = 0,shape=16,size=1) +  
+  scale_x_discrete(breaks=c(FALSE, TRUE),
+                   labels=c("Sparse","Abundant"))+
+  scale_fill_manual(labels = c('Sparse', 'Abundant'),values = c("#FFC19C", "#C59AD4"))+
+  labs(y ='Stability', x=NULL, fill=NULL)
+stab_plot
+
+# stats
+lmm = lmer(value ~ sal +(1|mouse)+(1|dataset), data =perf_data)
+anova(lmm)
+
 
 ## Bayesian vs random
-random = readMat(file.path(folder,"all_error_rand.mat"))
+random = readMat(file.path(folder,"all_obj_error_rand.mat"))
 
 perf_random = random[[1]]
 perf_random <- data.table( "value" = perf_random[,1], "dataset" = perf_random[,2], "env" = perf_random[,3])
@@ -105,7 +115,7 @@ perf_random$nov = perf_random$env >2
 perf_random$sal = perf_random$env ==2 | perf_random$env ==4
 perf_random$random = 1
 
-dataset = readMat(file.path(folder,"all_error.mat"))
+dataset = readMat(file.path(folder,"all_obj_error.mat"))
 
 perf_data = dataset[[1]]
 perf_data <- data.table( "value" = perf_data[,1], "dataset" = perf_data[,2], "env" = perf_data[,3])
@@ -115,45 +125,53 @@ perf_data$sal = perf_data$env ==2 | perf_data$env ==4
 perf_data$random = 0
 
 all_data = rbind(perf_data, perf_random)
-rand_plot = ggplot(data=all_data, aes(x=sal, y=value, fill=factor(random))) +
+rand_plot_ovc = ggplot(data=all_data, aes(x=sal, y=value, fill=factor(random))) +
   stat_summary(fun=mean, geom='bar', alpha=1, position = position_dodge(width=0.8), width=0.8) +
   stat_summary(fun.data = mean_cl_normal, geom="errorbar", width=0.3, position = position_dodge(width=0.8)) +
-  geom_point(position=position_jitterdodge(dodge.width = 0.8, jitter.width = 0.2), alpha=0.5, stroke = 0,shape=16,size=1) +
+  geom_point(position=position_jitterdodge(dodge.width = 0.8, jitter.width = 0.2),alpha=0.5, stroke = 0,shape=16,size=1) +  
   facet_wrap(~ nov, labeller = labeller(nov = nov_names)) +
-  scale_x_discrete(breaks=c(FALSE,TRUE),
-                   labels=c("Sparse","Abundant"))+ 
+  scale_x_discrete(breaks=c(FALSE, TRUE),
+                   labels=c("Sparse","Abundant")) + 
   scale_fill_manual(labels = c('Data', 'Shuffled'), values=c("#6962AD", "#96E9C6")) +
   labs(y ='Prediction error (cm)', x=NULL, fill=NULL)
-rand_plot
+rand_plot_ovc
 
+# stats
 lmm = lmer(value ~ sal*nov + random +(1|dataset), data =all_data)
 anova(lmm)
 
+for(n in 0:1)
+{
+  for(s in 0:1)
+  {
+    print(shapiro.test(all_data[all_data$nov == n&all_data$sal == s& all_data$random == 0]$value))
+    print(shapiro.test(all_data[all_data$nov == n&all_data$sal == s& all_data$random == 1]$value))
+  }
+}
 
-perf_data$random = factor(perf_data$random)
 stat.test <- all_data %>%
-  group_by(env) %>%
+  group_by(nov, sal) %>%
   wilcox_test(value~random, paired=TRUE) %>%
   adjust_pvalue(method = "BH") %>%
   add_significance()
 stat.test
-tt = as.data.frame(stat.test)
-write.csv(tt, "test.csv")
 
-
-plot_temp = (pf_plot|
+## Combine the plots
+plot_temp = (pc_v_ovc_plot|pf_plot|
                io_plot)/(
                  stab_plot|
-                   rand_plot)
+                   rand_plot_ovc)
 
 
 plot_all = plot_temp +
   plot_layout(guides = "collect") +
   plot_annotation(tag_levels = 'A')&
-  theme_classic()
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank())
+
 plot_all
 
 
-ggsave(file.path(out_folder,'sfig2_PCs_v2.png'), plot_all, height = 6, width = 8)
-ggsave(file.path(out_folder,'sfig2_PCs_v2.pdf'), plot_all, height = 6, width = 8)
+ggsave(file.path(out_folder,'sfig4_ovcs.png'), plot_all, height = 8, width = 12)
+ggsave(file.path(out_folder,'sfig4_ovcs.pdf'), plot_all, height = 8, width = 12)
 
